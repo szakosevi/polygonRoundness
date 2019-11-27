2a206470984425b083fa4868ad2804a71c24b075
szakosevi <szakosevi@gmail.com>
2019-11-27 14:14:21 +0000

    TODO: create a new object line by line

--- main.cpp
+++ main.cpp
@@ -24,11 +24,11 @@ istream& operator>>(istream& is, Point &p)
 }
 
 /*Calculate from points the original radius*/
-vector<vector<double>> original_radius_points(vector<vector<Point>> point_Data, Point p)
+vector<vector<double>> original_radius_points(vector<vector<Point>> pointData, Point p)
 {
-  vector<vector<double>> dis_Data;
+  vector<vector<double>> disData;
   double r;
-  for (auto &list : point_Data)
+  for (auto &list : pointData)
     {      
         vector<double> dis;
         for (auto &mdata : list)
@@ -36,16 +36,16 @@ vector<vector<double>> original_radius_points(vector<vector<Point>> point_Data, 
             r = p.distance(mdata);
             dis.push_back(r);
         }
-        dis_Data.push_back(dis);
+        disData.push_back(dis);
     }
-    return dis_Data;
+    return disData;
 }
 
 /*Calculate the avarage radius*/
-vector<double> avarage_radius(vector<vector<Point>> point_Data, Point p)
+vector<double> avarage_radius(vector<vector<Point>> pointData, Point p)
 {
-  vector<vector<double>> r_data = original_radius_points(point_Data,p);  
-  vector<double> r_av; 
+  vector<vector<double>> r_data = original_radius_points(pointData,p);  
+  vector<double> rAv; 
   double r;
   for (auto &list : r_data)
     {      
@@ -56,18 +56,18 @@ vector<double> avarage_radius(vector<vector<Point>> point_Data, Point p)
             r_sum += mdata;
         }
         av = r_sum/list.size();
-        r_av.push_back(av);
+        rAv.push_back(av);
     }
-    return r_av;
+    return rAv;
 }
 
 /*Search max radius*/
-vector<double> max_radius(vector<vector<Point>> point_Data, Point p)
+vector<double> max_radius(vector<vector<Point>> pointData, Point p)
 {
-  vector<vector<double>> r_data = original_radius_points(point_Data,p); 
+  vector<vector<double>> rData = original_radius_points(pointData,p); 
   double max;
-  vector<double> max_Data;
-  for (auto &list : r_data)
+  vector<double> maxData;
+  for (auto &list : rData)
     {      
         max = list[0];
         for (auto &mdata : list)
@@ -75,18 +75,18 @@ vector<double> max_radius(vector<vector<Point>> point_Data, Point p)
             if(max<mdata)
               max = mdata;
         }
-        max_Data.push_back(max);
+        maxData.push_back(max);
     }
-  return max_Data; 
+  return maxData; 
 }
 
 /*Search min radius*/
-vector<double> min_radius(vector<vector<Point>> point_Data, Point p)
+vector<double> min_radius(vector<vector<Point>> pointData, Point p)
 {
-  vector<vector<double>> r_data = original_radius_points(point_Data,p); 
+  vector<vector<double>> rData = original_radius_points(pointData,p); 
   double min;
-  vector<double> min_Data;
-  for (auto &list : r_data)
+  vector<double> minData;
+  for (auto &list : rData)
     {      
         min = list[0];
         for (auto &mdata : list)
@@ -94,21 +94,21 @@ vector<double> min_radius(vector<vector<Point>> point_Data, Point p)
             if(min>mdata)
               min = mdata;
         }
-        min_Data.push_back(min);
+        minData.push_back(min);
     }
-  return min_Data; 
+  return minData; 
 }
 
 /*Calculate the error beetween original radius and estimated radius*/
-vector<double> error_calculation(vector<double> r_Data, vector<double> min_max_Data)
+vector<double> error_calculation(vector<double> rData, vector<double> minMaxData)
 {
   vector<double> res;
   double tmp;
-  for(int i = 0; i < r_Data.size(); ++i) 
+  for(int i = 0; i < rData.size(); ++i) 
   {
-    for(int j = 0; j < min_max_Data.size(); ++j) 
+    for(int j = 0; j < minMaxData.size(); ++j) 
     {
-      tmp =r_Data[i]-min_max_Data[j];
+      tmp = rData[i]-minMaxData[j];
     } 
     res.push_back(tmp);  
   }  
@@ -116,20 +116,20 @@ vector<double> error_calculation(vector<double> r_Data, vector<double> min_max_D
 }
 
 /*Calculate radius difference in %: (orig radius - estimated radius)/ orig radius*/
-vector<double> error_percentage(vector<double> r_Data, vector<double> min_max_Data)
+vector<double> error_percentage(vector<double> rData, vector<double> minMaxData)
 {
   vector<double> res;
   double tmp;
-  for(int i = 0; i < r_Data.size(); ++i) 
+  for(int i = 0; i < rData.size(); ++i) 
   {
-    for(int j = 0; j < min_max_Data.size(); ++j) 
+    for(int j = 0; j < minMaxData.size(); ++j) 
     {
-      if(r_Data[i] == 0 || min_max_Data[j] == 0 )
+      if(rData[i] == 0 || minMaxData[j] == 0 )
         tmp = 0;
       else
         {
-          tmp =abs(r_Data[i]-min_max_Data[j]);
-          tmp = (tmp*100) /r_Data[i];
+          tmp =abs(rData[i]-minMaxData[j]);
+          tmp = (tmp*100) /rData[i];
         }
     } 
     res.push_back(tmp);  
@@ -138,30 +138,31 @@ vector<double> error_percentage(vector<double> r_Data, vector<double> min_max_Da
 }
 
 /*split vector<> and return with error value*/
-vector<vector<double>> split(vector<vector<double>> r_data, vector<vector<double>> min_max_Data)
+vector<vector<double>> split(vector<vector<double>> rData, vector<vector<double>> minMaxData)
 {
+    /*TODO: create an object line by line */
     vector<vector<double>> dummy;
-    vector<double> a = r_data[0];
-    vector<double> b = r_data[1];
-    vector<double> c = r_data[2];
-    vector<double> d = r_data[3];
-    vector<double> e = r_data[4];
-    vector<double> f = r_data[5];
-    vector<double> g = r_data[6];
-    vector<double> h = r_data[7];
-    vector<double> j = r_data[8];
+    vector<double> a = rData[0];
+    vector<double> b = rData[1];
+    vector<double> c = rData[2];
+    vector<double> d = rData[3];
+    vector<double> e = rData[4];
+    vector<double> f = rData[5];
+    vector<double> g = rData[6];
+    vector<double> h = rData[7];
+    vector<double> j = rData[8];
     /*vector<double> hh = r_data[9];
     vector<double> jh = r_data[10];*/
 
-    vector<double> am = min_max_Data[0];
-    vector<double> bm = min_max_Data[1];
-    vector<double> cm = min_max_Data[2];
-    vector<double> dm = min_max_Data[3];
-    vector<double> em = min_max_Data[4];
-    vector<double> fm = min_max_Data[5];
-    vector<double> gm = min_max_Data[6];
-    vector<double> hm = min_max_Data[7];
-    vector<double> jm = min_max_Data[8];
+    vector<double> am = minMaxData[0];
+    vector<double> bm = minMaxData[1];
+    vector<double> cm = minMaxData[2];
+    vector<double> dm = minMaxData[3];
+    vector<double> em = minMaxData[4];
+    vector<double> fm = minMaxData[5];
+    vector<double> gm = minMaxData[6];
+    vector<double> hm = minMaxData[7];
+    vector<double> jm = minMaxData[8];
     /*vector<double> hhm = min_max_Data[9];
     vector<double> jhm = min_max_Data[10];*/
     dummy.push_back(error_percentage(a,am));
@@ -196,62 +197,6 @@ vector<double> pass(vector<vector<double>> p)
   return pas;
 }
 
-/*Generate point with help of r radius*/
-vector<Point> generate_reference_circle_point(Point p0,double r)
-{
-  double x = r, y = 0;
-  vector<Point> p_data;
-  Point tmp;
-  tmp.setXY(x+p0.getX(),y+p0.getY());
-  p_data.push_back(tmp);
-  /*if(r>0)
-  { 
-    tmp.setXY(x+p0.getX(),-y+p0.getY());
-    p_data.push_back(tmp);
-    tmp.setXY(y+p0.getX(),x+p0.getY());
-    p_data.push_back(tmp);
-    tmp.setXY(-y+p0.getX(),x+p0.getY());
-    p_data.push_back(tmp);
-  }*/
-  double P = 1-r;
-  while(x>y)
-  {
-    y++;
-    if(P<=0) //inside
-      P = P + 2*y + 1;
-    else
-    {
-      x--; // outside
-       P = P + 2*y -2*x + 1;
-    }
-    if(x<y)
-      break;
-
-    tmp.setXY(x+p0.getX(),y+p0.getY());
-    p_data.push_back(tmp);
-    tmp.setXY(-x+p0.getX(),y+p0.getY());
-    p_data.push_back(tmp);
-    tmp.setXY(x+p0.getX(),-y+p0.getY());
-    p_data.push_back(tmp);
-    tmp.setXY(-x+p0.getX(),-y+p0.getY());
-    p_data.push_back(tmp);
-
-    if(x!=y)
-    {
-      tmp.setXY(y+p0.getX(),x+p0.getY());
-      p_data.push_back(tmp);
-      tmp.setXY(-y+p0.getX(),x+p0.getY());
-      p_data.push_back(tmp);
-      tmp.setXY(y+p0.getX(),-x+p0.getY());
-      p_data.push_back(tmp);
-      tmp.setXY(-y+p0.getX(),-x+p0.getY());
-      p_data.push_back(tmp);
-    }
-
-  }
-  return p_data;
-}
-
 /*Organise data in ascending order*/
 vector<size_t> sort_idx(vector<double> v)
 {
@@ -264,7 +209,7 @@ vector<size_t> sort_idx(vector<double> v)
 
 int main() {
   int idx = 0, idx2;
-  vector<vector<Point>> point_Data;
+  vector<vector<Point>> pointData;
   string line;
 
   ifstream myfile("polygons.in");
@@ -280,7 +225,7 @@ int main() {
       {
         point.push_back(p0);
       }
-      point_Data.push_back(point);
+      pointData.push_back(point);
       idx++;    
     }
     myfile.close();
@@ -290,34 +235,34 @@ int main() {
     cout << "Unable to open file";
 
   Point p00; // (0,0)
-  vector<double> size_Data;
-  vector<double> r = avarage_radius(point_Data,p00);
-  vector<vector<double>> r_data = original_radius_points(point_Data,p00);
-  vector<double> max_r = max_radius(point_Data,p00);
-  vector<double> min_r = min_radius(point_Data,p00);
-  vector<vector<double>> e_max,e_min, e_mean, e_perc;
+  vector<double> sizeData;
+  vector<double> r = avarage_radius(pointData,p00);
+  vector<vector<double>> rData = original_radius_points(pointData,p00);
+  vector<double> maxR = max_radius(pointData,p00);
+  vector<double> minR = min_radius(pointData,p00);
+  vector<vector<double>> errorMax,errorMin, errorMean, errorResult;
   vector<vector<Point>> p_data_gen;
   vector<Point> ref_gen;
   vector<size_t> stx;
 
-  for (auto &list : r_data)
+  for (auto &list : rData)
     { 
-      e_max.push_back(error_calculation(max_r, list));
+      errorMax.push_back(error_calculation(maxR, list));
     }
 
-  for (auto &list : r_data)
+  for (auto &list : rData)
     { 
-      e_min.push_back(error_calculation(min_r, list));
+      errorMin.push_back(error_calculation(minR, list));
     }
-  for (auto &list : r_data)
+  for (auto &list : rData)
     { 
-      e_mean.push_back(error_calculation(r, list));
+      errorMean.push_back(error_calculation(r, list));
     }
 #if PRINT
   cout << "POINTS:" << endl;
-  for (auto &list : point_Data)
+  for (auto &list : pointData)
     {
-      size_Data.push_back(list.size());
+      sizeData.push_back(list.size());
       
         for (auto &mdata : list)
         {
@@ -326,29 +271,27 @@ int main() {
         cout << endl;
     }
 #endif
-    e_perc = split(r_data, e_max); // calc error
-    vector<double> op = pass(e_perc); // calc similarity
+    errorResult = split(rData, errorMax); // calc error
+    vector<double> op = pass(errorResult); // calc similarity
 #if PRINT
     for (auto &id : op)
       cout << "Similarity [%]:" << id << '\t' << endl;
 #endif
 #if PRINT
-    for (auto &id : max_r)
+    for (auto &id : maxR)
       cout << "MAX radius:" << id << '\t' << "" << endl;
-    for (auto &id : min_r)
+    for (auto &id : minR)
       cout << "MIN radius:" << id << '\t' << "" << endl;
     
-    for (auto &x : r_data)
+    for (auto &x : rData)
     {      
         for (auto &mdata : x)
         {
             cout << "Orig Radius:" << mdata << '\t' << "" << endl;
-            //ref_gen =  generate_reference_circle_point(p00,list);
-            //p_data_gen.push_back(ref_gen);
         }
         cout << endl;
     }
-    for (auto &list : e_perc)
+    for (auto &list : errorResult)
     {      
         for (auto &mdata : list)
         {
@@ -357,8 +300,8 @@ int main() {
         cout << endl;
     }
 #endif
-    cout << "Roundness:" << endl;
+    cout << "Roundness in ascending order:" << endl;
     for (auto &id : sort_idx(op))
-      cout << id << ' ' << ">";
+      cout << id << ". (shape) " <<"> ";
   return 0;
 }
